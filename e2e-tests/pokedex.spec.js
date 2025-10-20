0ad5bcecf1c02941687fadbccc34dc0b13b3c295
shanmengqi <shanmengqi@gmail.com>
2025-10-20 18:31:03 +0800

    add e2e test to workflow

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('can navigate to a Pokemon page', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
